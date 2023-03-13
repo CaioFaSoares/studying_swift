12b43145b292bfcdede32242fdfa2dc5f6906ad6
Caio Soares <caiofasoares@gmail.com>
2023-03-13 14:18:00 -0300

    vehiclePurchase complete.

--- vehicle-purchase/Sources/VehiclePurchase/VehiclePurchase.swift
+++ vehicle-purchase/Sources/VehiclePurchase/VehiclePurchase.swift
@@ -1,24 +1,48 @@
 func canIBuy(vehicle: String, price: Double, monthlyBudget: Double) -> String {
 
-    let pricePerMonthToBeMet: Double = price / 5 * 12
-    let frugalBudget: Double = monthlyBudget * 1.1
+    let vehicleMonthlyPrice: Double = price / (12 * 5)
 
-    if frugalBudget > pricePerMonthToBeMet && monthlyBudget < pricePerMonthToBeMet {
-        return("You'd have to be frugal to get a" + vehicle)
-    } else if monthlyBudget > pricePerMonthToBeMet {
-        return("Great, you can afford" + vehicle)
+    if vehicleMonthlyPrice <= monthlyBudget * 1.1 && vehicleMonthlyPrice > monthlyBudget {
+        return "I'll have to be frugal if I want a \(vehicle)"
+    } else if vehicleMonthlyPrice <= monthlyBudget {
+        return "Yes! I'm getting a \(vehicle)"
     } else {
-        return("Sorry, but no" + vehicle + "to you...")
+        return "Darn! No \(vehicle) for me"
     }
 
 }
 
 func licenseType(numberOfWheels wheels: Int) -> String {
-  //fatalError("Please implement licenseType(numberOfWheels:) function")
-    return("")
+
+    switch wheels {
+        case 2...3:
+        return "You will need a motorcycle license for your vehicle"
+        case 4...6:
+        return "You will need an automobile license for your vehicle"
+        case 18:
+        return "You will need a commercial trucking license for your vehicle"
+        default:
+        return "We do not issue licenses for those types of vehicles"
+    }
+
 }
 
 func registrationFee(msrp: Int, yearsOld: Int) -> Int {
-  //fatalError("Please implement registrationFee(msrp:yearsOld:) function")
-    return(1)
+
+    let flatFee     = 25
+    let baseCost    = 25000
+    var price       = 0
+
+    if yearsOld >= 10 {
+        return flatFee
+    }
+
+    if msrp > baseCost {
+        price = msrp
+    } else {
+        price = baseCost
+    }
+
+    return (price - ((Int(Double(price) * 0.1))*yearsOld))/100
+
 }
